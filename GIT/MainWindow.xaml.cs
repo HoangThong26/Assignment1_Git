2f1940ed60982f9e9611585d96e36c204d4b2bfd
hiep06092005 <hiepvtpt123@gmail.com>
2026-01-27 16:40:18 +0700

    update button hello

--- GIT/MainWindow.xaml.cs
+++ GIT/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace git
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("xin chào");
+        }
     }
 }
