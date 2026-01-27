99e28a9b2f8372d2ad4d6a83a0356b16d3b4e3a1
HoangThong26 <hbt2k4@gmail.com>
2026-01-27 17:13:32 +0700

    Xin chao

--- App/MainWindow.xaml.cs
+++ App/MainWindow.xaml.cs
@@ -20,5 +20,9 @@ namespace App
         {
             InitializeComponent();
         }
+        private void HelloButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Xin chào");
+        }
     }
 }
--- App/obj/Debug/net8.0-windows/App.AssemblyInfo.cs
+++ App/obj/Debug/net8.0-windows/App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+726a02043d50b7be511a7acf2267b7759f6ffcd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
